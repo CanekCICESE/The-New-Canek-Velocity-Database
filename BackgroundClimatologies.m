edf394ff2b98c58b66bc2a5a6c0f495a795593af
CanekCICESE <CANEK.CICESE@gmail.com>
2024-10-23 17:12:40 -0700

    Add files via upload

---
+++ BackgroundClimatologies.m
@@ -0,0 +1,119 @@
+% 2023 no casos si no caso shilo el 76 para florida
+clear all; close all; clc
+%
+% Grid resolution
+vres = 20; hres = 0.03;
+% interpolation parameters for both sections
+params_climy = [75000, 900, 0.01];
+params_climf = [90000, 900, 0.01];
+
+% path to the data
+raiz = 'C:\Users\nucle\Tesis\Papers\Paper_I\Figures\Programas\CreaRepos\Data\BackgroundSections\';
+% ===============================================================
+
+close all
+figure('Position', [70 50 560 604.8889]);
+colormap(jet(40))
+
+
+% Mapping the climatology from the Yucatan section
+
+load([raiz, '\yuc_vel_clim_clean.mat']);
+
+% make the scatter plot with the normal-to-the-section velocity as color
+subplot(2, 2, 1)
+box on;
+scatter( Nxp(:),Nzp(:), 15,Nvp(:), 'filled' );
+hold on;
+clim([-0.1, 1.2]);
+set(gca, 'XtickLabels', {})
+ylabel('Depth (m)', 'interpreter','latex')
+set(gca, 'FontSize', 11);
+batsec('yuc', gca, 'k', 2);
+axis([-86.8 -84.87, -2100, 100]);
+
+% prepare the Yucatan grid Xy, Zy
+[~, ~, ~, xmapy, zmapy, celday, Xy, Zy, dayu]= prep_sec('yuc', vres, hres);
+
+An = Nvp(:) - mean(Nvp(:)); % Remove the overal spatial mean
+% Perform the objetctive mapping
+[ClimVy, ~, Err]=objan_campomedio_yuc2(An(:),Nxp(:),Nzp(:),params_climy, ...
+    xmapy,zmapy);
+
+ClimVy = ClimVy + mean(Nvp(:)); % add-up the spatial mean
+
+% plot the mapped climatological section: Yucatan
+Uy = nan(size(Xy));
+Uy(celday) = ClimVy(:);
+
+
+subplot(2, 2, 3)
+box on;
+pcolor(Xy, Zy, Uy); shading interp
+hold on;
+[cony, hhy]= contour(Xy, Zy, Uy, [1, 0.9, 0.7, 0.5, 0.3, 0.1, -0.05, 0, -0.1, -0.2], 'k');
+clabel(cony, hhy, 'fontsize', 8)
+clim([-0.1, 1.2]);
+ylabel('Depth (m)', 'interpreter','latex')
+set(gca, 'FontSize', 11);
+batsec('yuc', gca, 'k', 2);
+axis([-86.8 -84.87, -2100, 100]);
+xlabel('Longitude', 'interpreter','latex')
+
+%==============================================================
+
+% Mapping the climatology from the Florida section
+
+load([raiz, '\flo_vel_clim_clean.mat']);
+
+% make the scatter plot with the normal-to-the-section velocity as color
+subplot(2, 2, 2)
+box on;
+scatter( Nxp(:),Nzp(:), 15,Nvp(:), 'filled' );
+hold on;
+set(gca, 'XtickLabels', {}, 'YtickLabels', {})
+batsec('flo', gca, 'k', 2);
+axis([23.1, 24.67, -2100, 100]);
+clim([-0.1, 1.2]);
+set(gca, 'FontSize', 11);
+
+
+% prepare the Florida grid
+[mask, xx, yy, xmapf, zmapf, celdaf, Xf, Zf, dafl]= prep_sec('flo', vres, hres);
+
+An = Nvp(:) - mean(Nvp(:)); % Remove the overal spatial mean
+
+[ClimVf, ~, ~]=objan_campomedio_flo2(An(:),Nxp(:),Nzp(:),params_climf,...
+    xmapf,zmapf);
+
+ClimVf = ClimVf + mean(Nvp(:)); % add-up the spatial mean
+
+
+% plot the mapped climatological section: Yucatan
+
+Uf = nan(size(Xf));
+Uf(celdaf) = ClimVf(:);
+
+subplot(2, 2, 4)
+pcolor(Xf, Zf, Uf);  shading interp
+hold on;
+[cony, hhy]= contour(Xf, Zf, Uf, [1, 0.9, 0.7, 0.5, 0.3, 0.1, -0.05, 0, -0.1, -0.2], 'k');
+clabel(cony, hhy, 'fontsize', 8)
+batsec('flo', gca, 'k', 2);
+axis([23.1, 24.67, -2100, 100]);
+clim([-0.1, 1.2]);
+set(gca, 'YtickLabels', {})
+
+
+cb = colorbar;
+cb.Position = [0.8610 0.1213 0.0311 0.2145];
+cb.AxisLocation = 'in';
+cb.FontSize = 11;
+set(gca, 'FontSize', 11);
+ylabel(cb, '(m s$^{-1}$)', 'interpreter','latex', 'fontsize', 13)
+xlabel('Latitude', 'interpreter','latex')
+%==============================================================
+
+%
+set (findobj (gcf, '-property', 'interpreter'), 'interpreter','latex')
+set (findobj (gcf, '-property', 'ticklabelinterpreter'), 'ticklabelinterpreter','latex');
---
+++ DefRadiious.m
@@ -0,0 +1,137 @@
+% Estimations of the first baroclinic radious of deformation following (Chelton et al., 1998)
+%
+clear all; clc; close all;
+
+load C:\Users\nucle\Tesis\Papers\Paper_I\Figures\Programas\CreaRepos\Data\CTD\CTD_cnk_Hist.mat
+
+addpath(genpath('C:\Users\nucle\Tesis\Papers\Paper_I\Figures\Programas\CreaRepos\extrn'))
+
+Ro = [];
+lat = [];
+lon = [];
+depth = [];
+
+lances = unique(fe); % find unique dates 
+tags = ones(size(fe)); % this vector will have the indices or "tags" for each cast
+ncasts = length(lances); % number of single casts
+aa = 1;
+m = 2;
+
+for i = 1 : ncasts
+    index = (fe == lances(i)); % find positions of a given cast
+    tags(index) = i; % tag the given cast (they are sorted by date) 
+    %  Casts are labeled as 1, 2, 3 ... to "ncasts"
+    
+    sdu = sa(index); 
+    tdu = ct(index);
+    pdu = p(index);
+    ladu = lam(index);
+    inba = isnan(sdu + tdu + pdu + ladu);
+    
+    sdu(inba) = []; tdu(inba) = []; pdu(inba) = []; ladu(inba) = [];
+    % determine if the casts are "complete" in depth
+    if min(p(index)) <= 40 &  max(p(index)) >= 650  & max(p(index)) < 2100
+        
+    % interpolate every cast, just in case is not at the same vertical resolution   
+    % normally do
+    [N2, p_m] = gsw_Nsquared(sdu, tdu, pdu, ladu);
+    N2(N2<0) = 0;
+    N = sqrt(N2)*2*pi;
+    x1 = N; x2 = p_m;
+   
+    dz = 10;
+    x3 = (40:dz:max(p_m))';
+    x4 = interp1(x2, x1, x3);
+    %=============================================================================
+    % compute the Ro using the equation from (Chelton et al. 1998)
+    Ro(aa) = ( 1./( coriolis(nanmean(lam(:)))*m*pi ) * nansum(x4)*dz )  * 1e-3;
+    
+    lat(aa) = mean(lam(index));
+    lon(aa) = mean(lom(index));
+    depth(aa) = max(p_m);
+    mN(aa) = nansum(N);
+    dzz(aa) = dz;
+    aa = aa + 1;
+    
+    end
+end
+
+
+%%
+close all
+
+% figure;set(gcf, 'pos', [50 50 1414 738])
+% axes('pos', [0.5552 0.2290 0.3498 0.6960])
+[xv, yv] = muadro([-86.6296 -84.8879], [21.4141 22.0294]);
+inyu = inpolygon(lon, lat, xv, yv);
+
+plot(depth(inyu), Ro(inyu), 'ok', 'markerfacecolor',  rgb('RoyalBlue'))
+
+b = polyfit(depth(inyu), Ro(inyu), 3);
+yp = polyval(b, 600:2100);
+hold on;
+plot(600:2100, yp, 'b', 'linewidth', 3)
+
+rmyu = mean(yp)
+[min( Ro(inyu)) max( Ro(inyu))]
+
+
+%
+
+hold on;
+[xv, yv] = muadro([-82.1774 -80.4355], [22.9898 24.5000]);
+infl = inpolygon(lon, lat, xv, yv);
+
+plot(depth(infl),Ro(infl), 'sk', 'markerfacecolor', rgb('OrangeRed'), 'markersize', 9)
+
+b = polyfit(depth(infl), Ro(infl), 2);
+yp = polyval(b, 750:1600);
+hold on;
+plot(750:1600, yp, 'r', 'linewidth', 3)
+
+rmflo = mean(yp)
+[min( Ro(infl)) max( Ro(infl))]
+
+title( '\textbf{Rossby Radius of Deformation (Chelton et al., 1998)}' );
+
+xlabel('Profile depth (m)')
+ylabel('Rossby radius of deformation (km)')
+
+
+ax0(1) = plot(NaN,NaN, 'sk', 'markerfacecolor', rgb('OrangeRed'), 'markersize', 9);
+ax0(2) = plot(NaN,NaN, 'ok', 'markerfacecolor',  rgb('RoyalBlue'));
+
+
+ll = legend(ax0, {'Florida', 'Yucatan'}, 'Location', 'Best');
+
+
+Fs = findall(gcf,'-property','FontSize');
+for k = 1 : length(Fs)
+    if strcmp(get(Fs(k), 'type'), 'axes')
+        if Fs(k).FontSize < 12
+            Fs(k).FontSize = 12;
+        end
+    end
+end
+
+set(findall(gcf,'-property','Interpreter'),'Interpreter', 'latex');
+set(findall(gcf,'-property','TickLabelInterpreter'),'TickLabelInterpreter', 'latex');
+
+text(2078, 98.5564, '\textbf{(I)}','Interpreter', 'latex', 'fontsize', 17);
+
+%%
+% axes('pos', [0.753889674681754 0.234540527223454 0.147807705924052 0.19308951974927]);
+% box on;
+% 
+% 
+% plot(lon(infl), lat(infl), '.', 'color', rgb('OrangeRed'), 'markersize', 7);
+% hold on;
+% 
+% plot(lon(inyu), lat(inyu), '.', 'color',  rgb('RoyalBlue'), 'markersize', 7);
+% hold on;
+% 
+% % pongolfo(gca, rgb('black'), 0);
+% axis equal tight
+% axis([-87.3, -80.7, 20.5, 25])
+% text(-87.1071, 24.2678, '\textbf{(II)}','Interpreter', 'latex', 'fontsize', 17);
+% set(gca, 'YtickLabels', {}, 'XtickLabels', {})
